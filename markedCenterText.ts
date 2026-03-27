8c059d754907edb9e748532f7efb9e31489c849b
Dragmine149 <dan@thebanners.uk>
2026-03-27 21:58:53 +0000

    Restructor code for readability and fix other markdown elements not working

--- markedCenterText.ts
+++ markedCenterText.ts
@@ -35,51 +35,49 @@ export function markedCenterText(): MarkedExtension {
     hooks: {
       processAllTokens(tokens) {
         return tokens.map((token) => {
-          if (token.type === 'paragraph' || token.type === 'text' || token.type === 'heading') {
-            if ('text' in token && typeof token.text === 'string') {
-              const text = token.text;
-              let align = '';
-              let processedText = text;
+          const valid_type = token.type === 'paragraph' || token.type === 'text' || token.type === 'heading';
+          if (!valid_type) { return token; }
+          if (typeof token.text !== 'string') { return token; }
+          if (!token.text.startsWith('!')) { return token; }
 
-              if (text.startsWith('!')) {
-                const firstSpace = text.indexOf(' ');
-                const command = text.substring(0, firstSpace);
-                processedText = text.substring(firstSpace + 1);
+          const firstSpace = token.text.indexOf(' ');
+          const command = token.text.substring(0, firstSpace);
 
-                switch (command) {
-                  case '!c':
-                    align = 'center';
-                    break;
-                  case '!r':
-                    align = 'right';
-                    break;
-                }
-              }
-
-              if (align) {
-                token.text = processedText;
-                Object.assign(token, {
-                  meta: {
-                    align: align
-                  }
-                });
-              }
-            }
+          let align = '';
+          switch (command) {
+            case '!c':
+              align = 'center';
+              break;
+            case '!r':
+              align = 'right';
+              break;
           }
+
+          if (!align) { return token; }
+
+          Object.assign(token, {
+            text: token.text.substring(firstSpace + 1),
+            meta: {
+              align: align
+            }
+          });
+
           return token;
         });
       }
     },
     renderer: {
-      heading({ meta, depth, text }: HeadingCenter) {
+      heading({ meta, depth, tokens }: HeadingCenter) {
+        tokens.shift();
         const align = meta?.align;
         const alignAttr = align ? ` style="text-align: ${align};"` : '';
-        return `<h${depth}${alignAttr}>${text}</h${depth}>`;
+        return `<h${depth}${alignAttr}>${this.parser.parseInline(tokens)}</h${depth}>`;
       },
-      paragraph({ meta, text }: ParagraphCenter) {
+      paragraph({ meta, tokens }: ParagraphCenter) {
+        tokens.shift();
         const align = meta?.align;
         const alignAttr = align ? ` style="text-align: ${align};"` : '';
-        return `<p${alignAttr}>${text}</p>`;
+        return `<p${alignAttr}>${this.parser.parseInline(tokens)}</p>`;
       }
     },
   };
